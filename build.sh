69d923a18cd3b1f75802cd717ccb1ad8591c49d4
James Ward <james@jamesward.org>
2011-04-21 10:01:59 -0600

    adding manifest and swf

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -r bin
 mkdir bin
 /home/jamesw/flex_sdk_4.5.0.19786-air_sdk_2.6.0.19120/bin/mxmlc -load-config /home/jamesw/flex_sdk_4.5.0.19786-air_sdk_2.6.0.19120/frameworks/air-config.xml -output bin/app.swf src/app.mxml
 /home/jamesw/flex_sdk_4.5.0.19786-air_sdk_2.6.0.19120/bin/adt -package -target apk-debug -storetype pkcs12 -keystore a.p12 -storepass a bin/app.apk app.xml -C bin app.swf
